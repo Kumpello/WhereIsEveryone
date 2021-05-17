558030dbf198bb5b93d580e09bcce574b9ca203f
Michał Kukulski <bleidd04@gmail.com>
2021-05-17 11:28:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/whereiseveryone/Login.java
@@ -0,0 +1,87 @@
+package com.example.whereiseveryone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+public class Login extends AppCompatActivity {
+
+    TextInputEditText textInputEditTextusername, textInputEditTextpassword;
+    Button login;
+    TextView textViewLSignUp;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        textInputEditTextusername = findViewById(R.id.username);
+        textInputEditTextpassword = findViewById(R.id.password);
+        login = findViewById(R.id.btnLogin);
+        textViewLSignUp = findViewById(R.id.signUpText);
+        progressBar = findViewById(R.id.progressBar);
+
+        textViewLSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username, password;
+                username = String.valueOf(textInputEditTextusername.getText());
+                password = String.valueOf(textInputEditTextpassword.getText());
+                if (!username.equals("") && !password.equals("") && !username.equals("")){
+                    progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] field = new String[2];
+                            field[0] = "username";
+                            field[1] = "password";
+                            String[] data = new String[2];
+                            data[0] = username;
+                            data[1] = password;
+                            PutData putData = new PutData("http://192.168.8.101/WhereIsEveryone/login.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if (result.equals("Login Success")) {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), Map.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), "All fields required", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/whereiseveryone/Map.java
@@ -0,0 +1,331 @@
+package com.example.whereiseveryone;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Looper;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Map extends FragmentActivity implements OnMapReadyCallback, SensorEventListener {
+
+    private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location userLocation;
+    private LocationRequest userLocationRequest;
+    private Resources resources;
+    private Timer timer;
+    private Marker userMarker;
+    private SensorManager sensorManager;
+    private boolean requestingLocationUpdates;
+    private final float[] accelerometerReading = new float[3];
+    private final float[] magnetometerReading = new float[3];
+    private final float[] rotationMatrix = new float[9];
+    private final float[] orientationAngles = new float[3];
+    private LocationCallback locationCallback;
+    private float azimuth;
+    private ImageButton getLocationButton;
+
+    final private int FASTEST_INTERVAL = 500;
+    final private int INTERVAL = 2000;
+    final private float m = (float) (180 / Math.PI);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        userLocationRequest = LocationRequest.create();
+        userLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        userLocationRequest.setInterval(INTERVAL);
+        userLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        resources = getResources();
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        timer = new Timer();
+        requestingLocationUpdates = true;
+        getLocationButton = findViewById(R.id.getLocation);
+
+
+        getLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                centerCamera();
+            }
+        });
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull @org.jetbrains.annotations.NotNull LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                userLocation = locationResult.getLastLocation();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        updateOrientationAngles();
+                        azimuth = getAzimuth(orientationAngles[0]);
+                        userMarker.setPosition(new LatLng(userLocation.getLatitude(), userLocation.getLongitude()));
+                        userMarker.setRotation(azimuth);
+                        Toast.makeText(getApplicationContext(), String.valueOf(orientationAngles[0] * 100), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        };
+
+
+        askForPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, resources.getString(R.string.location_permission_text));
+        askForPermissions(Manifest.permission.ACCESS_FINE_LOCATION, resources.getString(R.string.location_permission_text));
+
+        startLocationUpdates();
+
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                getLastLocation();
+                updateOrientationAngles();
+                azimuth = getAzimuth(orientationAngles[0]);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        userMarker = mMap.addMarker(new MarkerOptions().position(
+                                new LatLng(userLocation.getLatitude(), userLocation.getLongitude()))
+                                .flat(true)
+                                .anchor(0.5f,0.5f)
+                                .rotation(azimuth).visible(true)
+                                .icon(BitmapDescriptorFactory.fromBitmap(getBitmapFromVectorDrawable(getApplicationContext(), R.drawable.ic_round_navigation_24))));
+                    }
+                });
+                centerCamera();
+            }
+        }, 500);
+
+    }
+
+    public float getAzimuth(float orientationAngle) {
+        if (orientationAngles[0] >= 0) {
+            return orientationAngles[0] * m;
+        } else {
+            return 360 + (orientationAngles[0] * m);
+        }
+    }
+
+    public Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
+        Drawable drawable = ContextCompat.getDrawable(context, drawableId);
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+
+
+    private void centerCamera() {
+        if (userLocation != null) {
+            getLastLocation();
+            updateOrientationAngles();
+            LatLng userLocationLatLng = new LatLng(userLocation.getLatitude(), userLocation.getLongitude());
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(userLocationLatLng)
+                            .zoom(15)
+                            .bearing(orientationAngles[0])
+                            .build();
+                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        }
+        Sensor magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        if (magneticField != null) {
+            sensorManager.registerListener(this, magneticField,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        }
+        if (requestingLocationUpdates) {
+            startLocationUpdates();
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(userLocationRequest,
+                locationCallback,
+                Looper.getMainLooper());
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Don't receive any more updates from either sensor.
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopLocationUpdates();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading,
+                    0, accelerometerReading.length);
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading,
+                    0, magnetometerReading.length);
+        }
+    }
+
+    public void updateOrientationAngles() {
+        // Update rotation matrix, which is needed to update orientation angles.
+        SensorManager.getRotationMatrix(rotationMatrix, null,
+                accelerometerReading, magnetometerReading);
+        // "rotationMatrix" now has up-to-date information.
+        SensorManager.getOrientation(rotationMatrix, orientationAngles);
+        // "orientationAngles" now has up-to-date information.
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void askForPermissions(String permission, String message) {
+        int requestCode = 0;
+        switch (permission) {
+            case Manifest.permission.ACCESS_FINE_LOCATION:
+                requestCode = 1;
+                break;
+            case Manifest.permission.ACCESS_COARSE_LOCATION:
+                requestCode = 2;
+                break;
+        }
+        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED) {
+            getLastLocation();
+        } else if (shouldShowRequestPermissionRationale(permission)) {
+            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+            builder.setTitle(resources.getString(R.string.permission_needed));
+            builder.setMessage(message);
+            builder.setNegativeButton(resources.getString(R.string.decline), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }});
+            builder.setPositiveButton(resources.getString(R.string.accept), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    requestPermissions(new String[]{permission}, 1);
+                    dialog.dismiss();
+                }});
+            builder.show();
+        } else {
+            requestPermissions(new String[] {permission}, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1 || requestCode == 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+                centerCamera();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation(){
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            userLocation = location;
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+
+        getLastLocation();
+        if (userLocation != null) {
+            LatLng userLocationLatLng = new LatLng(userLocation.getLatitude(), userLocation.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocationLatLng));
+        }
+    }
+}
