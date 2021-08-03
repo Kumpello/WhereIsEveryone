bc03a064340c341b9704c10fc17dafbb8c2e3f16
Kumpello <bleidd04@gmail.com>
2021-08-03 17:48:53 +0200

    -added navigation for bottom bar

--- app/src/main/java/com/example/whereiseveryone/view/MainActivity.java
+++ app/src/main/java/com/example/whereiseveryone/view/MainActivity.java
@@ -1,18 +1,38 @@
 package com.example.whereiseveryone.view;
 
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.whereiseveryone.R;
+import com.example.whereiseveryone.databinding.ActivityMainBinding;
 import com.example.whereiseveryone.mvp.BaseActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 //What to do with this error?
 public class MainActivity extends BaseActivity {
 
+    private NavController navController;
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+
+        navController = navHostFragment.getNavController();
+
+        BottomNavigationView bottomNav = binding.bottomNavigation;
+        NavigationUI.setupWithNavController(bottomNav, navController);
 
     }
 }
